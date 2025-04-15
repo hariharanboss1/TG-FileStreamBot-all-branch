fbfa9eb9e3e1c9e08fce75f595895894dac6b0eb
hariharanboss1 <167068954+hariharanboss1@users.noreply.github.com>
2025-04-15 17:28:48 +0530

    Update commands.go

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"EverythingSuckz/fsb/internal/analytics"
 	"reflect"
+	"time"
 
 	"github.com/celestix/gotgproto/dispatcher"
 	"go.uber.org/zap"
@@ -14,6 +16,14 @@ type command struct {
 func Load(log *zap.Logger, dispatcher dispatcher.Dispatcher) {
 	log = log.Named("commands")
 	defer log.Info("Initialized all command handlers")
+	
+	// Initialize analytics service
+	analyticsService := analytics.NewAnalyticsService(5*time.Minute, 1000)
+	
+	// Register analytics commands
+	analytics.RegisterTelegramCommands(dispatcher, analyticsService)
+	
+	// Load other commands
 	Type := reflect.TypeOf(&command{log})
 	Value := reflect.ValueOf(&command{log})
 	for i := 0; i < Type.NumMethod(); i++ {
