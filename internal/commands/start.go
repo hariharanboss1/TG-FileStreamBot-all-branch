34b17f126de8b2f9cf26211241d8a7117fdc5ec8
hariharanboss1 <167068954+hariharanboss1@users.noreply.github.com>
2025-05-19 00:08:24 +0530

    Update start.go

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -8,6 +8,8 @@ import (
 	"github.com/celestix/gotgproto/dispatcher/handlers"
 	"github.com/celestix/gotgproto/ext"
 	"github.com/celestix/gotgproto/storage"
+	"github.com/celestix/gotgproto/tg"
+	"fmt"
 )
 
 func (m *command) LoadStart(dispatcher dispatcher.Dispatcher) {
@@ -26,6 +28,33 @@ func start(ctx *ext.Context, u *ext.Update) error {
 		ctx.Reply(u, "You are not allowed to use this bot.", nil)
 		return dispatcher.EndGroups
 	}
+
+	// Check if force sub is enabled and user is subscribed
+	if config.ValueOf.ForceSubChannelID != 0 {
+		isSubscribed, err := utils.IsUserSubscribed(ctx, ctx.Client, chatId)
+		if err != nil {
+			ctx.Reply(u, "Error checking subscription status. Please try again later.", nil)
+			return dispatcher.EndGroups
+		}
+		if !isSubscribed {
+			row := tg.KeyboardButtonRow{
+				Buttons: []tg.KeyboardButtonClass{
+					&tg.KeyboardButtonURL{
+						Text: "Join Channel",
+						URL:  fmt.Sprintf("https://t.me/%d", config.ValueOf.ForceSubChannelID),
+					},
+				},
+			}
+			markup := &tg.ReplyInlineMarkup{
+				Rows: []tg.KeyboardButtonRow{row},
+			}
+			ctx.Reply(u, "Please join our channel to use this bot.", &ext.ReplyOpts{
+				Markup: markup,
+			})
+			return dispatcher.EndGroups
+		}
+	}
+
 	ctx.Reply(u, "Need a direct streamable link to a file? Send it my way! 🤓\n\nJoin my Update Channel @haris_garage 🗿 for more updates.\n\nLink validity: 24 hours ⏳\n\nPro Tip: Use 1DM Browser for lightning-fast downloads! 🔥", nil)
 	return dispatcher.EndGroups
 }
